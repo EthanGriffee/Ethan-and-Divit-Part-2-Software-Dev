686261de124052bebdd0743b15f33ed4bc5ed4e0
Ethan Griffee <griffee.e@husky.neu.edu>
2020-01-29 23:56:14 -0500

    added error testing using google test

--- test-array.cpp
+++ test-array.cpp
@@ -1,12 +1,17 @@
+#include <gtest/gtest.h>
+
 #include "object.h"  
 #include "string.h"  
 #include "array.h"    
-#include "helper.h"
- 
-void FAIL() {   exit(1);    }
-void OK(const char* m) { println(m);}
-void t_true(bool p) { if (!p) FAIL(); }
-void t_false(bool p) { if (p) FAIL(); }
+
+#define CS4500_ASSERT_TRUE(a)  \
+  ASSERT_EQ((a),true);
+#define CS4500_ASSERT_FALSE(a) \
+  ASSERT_EQ((a),false);
+#define CS4500_ASSERT_EXIT_ZERO(a)  \
+  ASSERT_EXIT(a(), ::testing::ExitedWithCode(0), ".*");
+#define CS4500_ASSERT_EXIT_ONE(a)  \
+  ASSERT_EXIT(a(), ::testing::ExitedWithCode(1), ".*");
  
 // Tests setting, getting, size, and push
 void test1() {
@@ -17,16 +22,20 @@ void test1() {
   a->push(s);
   a->push(t);
   a->push(x);
-  t_true(a->get(2)->equals(x));
-  t_true(a->get(1)->equals(t));
-  t_true(a->size() == 2);
+  CS4500_ASSERT_TRUE(a->get(2)->equals(x));
+  CS4500_ASSERT_TRUE(a->get(1)->equals(t));
+  CS4500_ASSERT_TRUE(a->size() == 2);
   a->set(1, s);
-  t_true(a->get(1)->equals(s));
+  CS4500_ASSERT_TRUE(a->get(1)->equals(s));
   delete s;
   delete t;
   delete x;
   delete a;
-  OK("1");
+  exit(0);
+}
+
+TEST(W1, test1) {
+  CS4500_ASSERT_EXIT_ZERO(test1)
 }
  
 // Tests indexOf and clear
@@ -38,18 +47,22 @@ void test2() {
   a->push(s);
   a->push(t);
   a->push(u);
-  t_true(a->indexOf(t) == 1);
-  t_true(a->indexOf(s) == 0);
-  t_true(a->indexOf(s, 1) == -1);
+  CS4500_ASSERT_TRUE(a->indexOf(t) == 1);
+  CS4500_ASSERT_TRUE(a->indexOf(s) == 0);
+  CS4500_ASSERT_TRUE(a->indexOf(s, 1) == -1);
   a->set(2, s);
-  t_true(a->indexOf(s, 1) == 2);
+  CS4500_ASSERT_TRUE(a->indexOf(s, 1) == 2);
   a->clear();
-  t_true(a->indexOf(t) == -1);
+  CS4500_ASSERT_TRUE(a->indexOf(t) == -1);
   delete s;
   delete t;
   delete u;
   delete a;
-  OK("2");
+  exit(0);
+}
+
+TEST(W1, test2) {
+  CS4500_ASSERT_EXIT_ZERO(test2)
 }
  
 // Tests concat and equals
@@ -65,18 +78,18 @@ void test3() {
   a2->push(u);
   a2->push(o);
   Array * a3 = a->concat(a2);
-  t_true(a3->indexOf(t) == 1);
-  t_true(a3->indexOf(o) == 3);
-  t_false(a3->equals(a2));
-  t_false(a3->equals(u));
-  t_false(a->equals(a2));
-  t_true(a3->size() == 4);
+  CS4500_ASSERT_TRUE(a3->indexOf(t) == 1);
+  CS4500_ASSERT_TRUE(a3->indexOf(o) == 3);
+  CS4500_ASSERT_FALSE(a3->equals(a2));
+  CS4500_ASSERT_FALSE(a3->equals(u));
+  CS4500_ASSERT_FALSE(a->equals(a2));
+  CS4500_ASSERT_TRUE(a3->size() == 4);
   Array * a4 = new Array(4);
   a4->push(s);
   a4->push(t);
   a4->push(u);
   a4->push(o);
-  t_true(a3->equals(a4));
+  CS4500_ASSERT_TRUE(a3->equals(a4));
   delete s;
   delete t;
   delete u;
@@ -85,8 +98,10 @@ void test3() {
   delete a2;
   delete a3;
   delete a4;
-  OK("3");
+  exit(0);
 }
+
+TEST(W1, test3) { CS4500_ASSERT_EXIT_ZERO(test3) }
  
 // tests the hashing
 void test4() {
@@ -94,28 +109,35 @@ void test4() {
   String * t = new String("B");
   Array * a = new Array(2);
   a->push(s);
-  t_true(a->hash() == s->hash());
+  CS4500_ASSERT_TRUE(a->hash() == s->hash());
   a->push(t);
-  t_true(a->hash() == s->hash() + t->hash());
+  CS4500_ASSERT_TRUE(a->hash() == s->hash() + t->hash());
   delete a;
   delete s;
   delete t;
-  OK("4");
+  exit(0);
 }
+
+TEST(W1, test4) { CS4500_ASSERT_EXIT_ZERO(test4) }
 
 //testing String Array
 void test5() {
   String * s = new String("A");
   String * t = new String("BCD");
+  String * e = new String("BCD");
   StringArray * a = new StringArray();
   a->push(s);
   a->push(t);
-  t_true(static_cast<String*> (a->get(1))->length() == 3);
+  CS4500_ASSERT_TRUE(static_cast<String*> (a->get(1))->equals(e));
+  a->set(0, t);
+  CS4500_ASSERT_TRUE(static_cast<String*> (a->get(0))->equals(t));
   delete a;
   delete s;
   delete t;
-  OK("5");
+  exit(0);
 }
+
+TEST(W1, test5) { CS4500_ASSERT_EXIT_ZERO(test5) }
 
 // testing Bool Array
 void test6() {
@@ -124,12 +146,16 @@ void test6() {
   BoolObj * b2 = new BoolObj(false);
   a->push(b1);
   a->push(b2);
-  t_true(static_cast<BoolObj*> (a->get(1))->getBool() == false);
+  CS4500_ASSERT_TRUE(static_cast<BoolObj*> (a->get(1))->getBool() == false);
+  a->set(0, b2);
+  CS4500_ASSERT_TRUE(static_cast<BoolObj*> (a->get(0))->getBool() == false);
   delete a;
   delete b1;
   delete b2;
-  OK("6");
+  exit(0);
 }
+
+TEST(W1, test6) { CS4500_ASSERT_EXIT_ZERO(test6) }
 
 // testing Int Array
 void test7() {
@@ -138,12 +164,17 @@ void test7() {
   IntObj * i2 = new IntObj(2);
   a->push(i1);
   a->push(i2);
-  t_true(static_cast<IntObj*> (a->get(1))->getInt() == 2);
+  CS4500_ASSERT_TRUE(static_cast<IntObj*> (a->get(1))->getInt() == 2);
+  a->set(0, i2);
+  CS4500_ASSERT_TRUE(static_cast<IntObj*> (a->get(0))->getInt() == 2);
   delete a;
   delete i1;
   delete i2;
-  OK("7");
+  exit(0);
 }
+
+
+TEST(W1, test7) { CS4500_ASSERT_EXIT_ZERO(test7) }
 
 // testing Float Array
 void test8() {
@@ -154,12 +185,14 @@ void test8() {
   a->push(f2);
   size_t epsilon = .01;
   float x = static_cast<FloatObj*> (a->get(1))->getFloat() - 2.2;
-  t_true( x < epsilon && x > epsilon); 
+  CS4500_ASSERT_TRUE( x < epsilon && x > epsilon); 
   delete a;
   delete f1;
   delete f2;
-  OK("8");
+  exit(0);
 }
+
+TEST(W1, test8) { CS4500_ASSERT_EXIT_ZERO(test8) }
 
 // tests the resizing
 void test9() {
@@ -173,26 +206,154 @@ void test9() {
   a->push(t);
   a->push(f);
   a->push(i);
-  t_true(a->get(3)->equals(i2));
+  CS4500_ASSERT_TRUE(a->get(3)->equals(i2));
   delete a;
   delete s;
   delete f;
   delete t;
   delete i;
   delete i2;
-  OK("9");
+  exit(0);
 }
 
+TEST(W1, test9) { CS4500_ASSERT_EXIT_ZERO(test9) }
+
+
+// test death of get
+void death_get() {
+  String * s = new String("Hello");
+  String * t = new String("World");
+  String * x = new String("HelloWorld");
+  Array * a = new Array(3);
+  a->push(s);
+  a->push(t);
+  a->push(x);
+  a->get(4);
+  exit(0);
+}
+
+TEST(D1, death_get) { CS4500_ASSERT_EXIT_ONE(death_get) }
+
+
+// test death of set
+void death_set() {
+  String * s = new String("Hello");
+  String * t = new String("World");
+  String * x = new String("HelloWorld");
+  Array * a = new Array(3);
+  a->push(s);
+  a->push(t);
+  a->push(x);
+  a->set(4, s);
+  exit(0);
+}
+
+TEST(D1, death_set) { CS4500_ASSERT_EXIT_ONE(death_set) }
+
+void typed_array_death_test_helper_push(Array* arr) {
+  String * s = new String("Hello");
+  FloatObj * f = new FloatObj(2.2);
+  arr->push(s);
+  arr->push(f);
+}
+
+// tests death of string array with push
+void death_push_string_arr_test() {
+  StringArray * a = new StringArray();
+  typed_array_death_test_helper_push(a);
+  exit(0);
+}
+
+TEST(D1, death_push_string_arr_test) { CS4500_ASSERT_EXIT_ONE(death_push_string_arr_test) }
+
+// tests death of string array with push
+void death_push_bool_arr_test() {
+  BoolArray * b = new BoolArray();
+  typed_array_death_test_helper_push(b);
+  exit(0);
+}
+
+TEST(D1, death_push_bool_arr_test) { CS4500_ASSERT_EXIT_ONE(death_push_bool_arr_test) }
+
+// tests death of int array with push
+void death_push_int_arr_test() {
+  IntArray * i = new IntArray();
+  typed_array_death_test_helper_push(i);
+  exit(0);
+}
+
+TEST(D1, death_push_int_arr_test) { CS4500_ASSERT_EXIT_ONE(death_push_int_arr_test) }
+
+// tests death of float array with push
+void death_push_float_arr_test() {
+  FloatArray * f = new FloatArray();
+  typed_array_death_test_helper_push(f);
+  exit(0);
+}
+
+TEST(D1, death_push_float_arr_test) { CS4500_ASSERT_EXIT_ONE(death_push_float_arr_test) }
+
+void typed_array_death_test_helper_set(Array* arr) {
+  String * s = new String("Hello");
+  FloatObj * f = new FloatObj(2.2);
+  arr->set(0, s);
+  arr->set(1, f);
+}
+
+// tests death of string array with set
+void death_set_string_arr_test() {
+  String * s = new String("A");
+  String * t = new String("BCD");
+  StringArray * a = new StringArray();
+  a->push(s);
+  a->push(t);
+  typed_array_death_test_helper_set(a);
+  exit(0);
+}
+
+TEST(D1, death_set_string_arr_test) { CS4500_ASSERT_EXIT_ONE(death_set_string_arr_test) }
+
+// tests death of float array with set
+void death_set_float_arr_test() {
+  FloatArray * a = new FloatArray();
+  FloatObj * f1 = new FloatObj(1.2);
+  FloatObj * f2 = new FloatObj(2.2);
+  a->push(f1);
+  a->push(f2);
+  typed_array_death_test_helper_set(a);
+  exit(0);
+}
+
+TEST(D1, death_set_float_arr_test) { CS4500_ASSERT_EXIT_ONE(death_set_float_arr_test) }
+
+// tests death of int array with set
+void death_set_int_arr_test() {
+  IntArray * a = new IntArray();
+  IntObj * i1 = new IntObj(12);
+  IntObj * i2 = new IntObj(2);
+  a->push(i1);
+  a->push(i2);
+  typed_array_death_test_helper_set(a);
+  exit(0);
+}
+
+TEST(D1, death_set_int_arr_test) { CS4500_ASSERT_EXIT_ONE(death_set_int_arr_test) }
+
+ // tests death of bool array with set
+void death_set_bool_arr_test() {
+  BoolArray * a = new BoolArray();
+  BoolObj * b1 = new BoolObj(true);
+  BoolObj * b2 = new BoolObj(false);
+  a->push(b1);
+  a->push(b2);
+  typed_array_death_test_helper_set(a);
+  exit(0);
+}
+
+TEST(D1, death_set_bool_arr_test) { CS4500_ASSERT_EXIT_ONE(death_set_bool_arr_test) }
+
  
-int main() {
-  test1();
-  test2();
-  test3();
-  test4();
-  test5();
-  test6();
-  test7();
-  test8();
-  test9();
-  return 0;
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
